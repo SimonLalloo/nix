d8078c518a21c62c5d92643a1cda3f628ce29f6d
Simon Lalloo <simon@lalloo.me>
2025-09-16 10:23:03 +0200

    Add Eduroam configuration

--- nixosModules/services/networking.nix
+++ nixosModules/services/networking.nix
@@ -25,6 +25,33 @@ with lib;
           "ipv6.method" = "auto";
           "ipv6.may-fail" = true;
         };
+
+        # Configuration for Eduroam
+        ensureProfiles.profiles = {
+          "eduroam" = {
+            connection = {
+              id = "eduroam";
+              type = "wifi";
+              autoconnect = true;
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "eduroam";
+            };
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-eap";
+            };
+            "802-1x" = {
+              eap = "peap";
+              identity = "sila3085@user.uu.se";
+              password = "";
+              phase2-auth = "mschapv2";
+            };
+            ipv4 = { method = "auto"; };
+            ipv6 = { method = "auto"; };
+          };
+        };
       };
     };
 
